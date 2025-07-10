afdd15ed24d8e06759b8dea6475663d602f86dde
vmr7999 <vmr7999@nyu.edu>
2025-07-10 00:40:36 -0400

    DNSServer.py

--- DNSServer.py
+++ DNSServer.py
@@ -54,7 +54,7 @@ password = 'vmr7999@nyu.edu'
 input_string = 'AlwaysWatching'
 
 encrypted_value = encrypt_with_aes(input_string, password, salt) # exfil function
-# decrypted_value = decrypt_with_aes(encrypted_value, password, salt)  # exfil function
+decrypted_value = decrypt_with_aes(encrypted_value, password, salt)  # exfil function
 
 # For future use    
 def generate_sha256_hash(input_string):
