ad67d434a66033bb89312a897b1b7a1423cd5608
vmr7999 <vmr7999@nyu.edu>
2025-07-11 12:08:04 -0400

    DNSServer.py

--- DNSServer.py
+++ DNSServer.py
@@ -49,7 +49,6 @@ password = 'vmr7999@nyu.edu'
 input_string = 'AlwaysWatching'
 
 encrypted_value = encrypt_with_aes(input_string, password, salt) # exfil function
-encrypted_string = encrypted_value.decode()
 decrypted_value = decrypt_with_aes(encrypted_value, password, salt)  # exfil function
 
 # For future use    
@@ -93,7 +92,7 @@ dns_records = {
     },
     'nyu.edu.': {
         dns.rdatatype.A: '192.168.1.106',
-        dns.rdatatype.TXT: TXT(dns.rdataclass.IN, dns.rdatatype.TXT, [encrypted_string])
+        dns.rdatatype.TXT: TXT(dns.rdataclass.IN, dns.rdatatype.TXT, [encrypted_value.decode('utf-8')]),
         dns.rdatatype.MX: [(10, 'mxa-00256a01.gslb.pphosted.com.')],
         dns.rdatatype.AAAA: '2001:0db8:85a3:0000:0000:8a2e:0373:7312',
         dns.rdatatype.NS: 'ns1.nyu.edu.',
