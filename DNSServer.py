15cb0c85fe92b624969e44e7e3522b62ab2c4a2c
vmr7999 <vmr7999@nyu.edu>
2025-07-10 22:32:50 -0400

    DNSServer.py

--- DNSServer.py
+++ DNSServer.py
@@ -16,7 +16,6 @@ import hashlib
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-from dns.rdtypes.ANY.TXT import TXT
 import base64
 import ast
 
